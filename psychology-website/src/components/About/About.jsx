a27a70676f8ec1637141e5c9e84d91260f923fa1
IsraelHumbertoLP <israel.pereira@corebiz.ag>
2023-06-13 00:57:42 -0300

    fix: remove console

--- psychology-website/src/components/About/About.jsx
+++ psychology-website/src/components/About/About.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 import styles from './About.module.scss'
 
 const About = ({about}) => {
-    console.log(about);
-
     const paragraphs = about.text.map(item => <p key={item._key}>{item.name}</p>)
     const itemsList = about.specialties.map(item => <li key={item._key} className={styles.item}>{item.Nome}</li>)
 
